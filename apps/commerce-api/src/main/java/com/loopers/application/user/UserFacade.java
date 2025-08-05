7c0675c0f0ea863b6992a9b03f178073a0bd8681
su <su@herit.net>
2025-08-06 00:23:43 +0900

    feat: merge UserFacade

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
@@ -1,9 +1,15 @@
 package com.loopers.application.user;
 
 import com.loopers.domain.user.UserModel;
+import com.loopers.domain.user.UserRepository;
 import com.loopers.domain.user.UserService;
+import com.loopers.domain.user.embeded.UserLoginId;
 import com.loopers.interfaces.api.user.UserV1Dto;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class UserFacade {
