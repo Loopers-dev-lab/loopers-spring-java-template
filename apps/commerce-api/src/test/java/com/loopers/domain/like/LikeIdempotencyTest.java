95fb703612353335c51e92d198e1a6f92591bfe8
su <su@herit.net>
2025-08-08 01:10:50 +0900

    refactor : brand, product 패키지 분리

--- apps/commerce-api/src/test/java/com/loopers/domain/like/LikeIdempotencyTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/like/LikeIdempotencyTest.java
@@ -1,5 +1,7 @@
 package com.loopers.domain.like;
 
+import com.loopers.domain.like.brand.BrandLikeModel;
+import com.loopers.domain.like.product.ProductLikeModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
