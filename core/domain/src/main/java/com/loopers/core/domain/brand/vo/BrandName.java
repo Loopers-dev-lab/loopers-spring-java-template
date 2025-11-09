e7a48b5e6b530758bbcf99e1967cf95bc25f2d01
kilian <audrb96@gmail.com>
2025-11-09 13:51:01 +0900

    feature : 브랜드명 Null 체크

--- core/domain/src/main/java/com/loopers/core/domain/brand/vo/BrandName.java
+++ core/domain/src/main/java/com/loopers/core/domain/brand/vo/BrandName.java
@@ -1,4 +1,19 @@
 package com.loopers.core.domain.brand.vo;
 
+import com.loopers.core.domain.error.DomainErrorCode;
+
+import java.util.Objects;
+
 public record BrandName(String value) {
+
+    public static BrandName create(String value) {
+        validateNotNull(value);
+
+        return new BrandName(value);
+    }
+
+    private static void validateNotNull(String value) {
+        Objects.requireNonNull(value, DomainErrorCode.notNullMessage("브랜드명"));
+    }
+
 }
