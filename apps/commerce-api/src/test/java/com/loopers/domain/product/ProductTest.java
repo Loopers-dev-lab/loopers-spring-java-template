1d1e1789b1a7da4b287215dc0cd4846e5463226f
HongChangMo <hcm7402@naver.com>
2025-11-10 08:46:46 +0900

    feat: Round 3 도메인 생성

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/product/ProductTest.java
@@ -0,0 +1,17 @@
+package com.loopers.domain.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+        Product pro = new Product();
+        // when
+        pro.add();
+        // then
+
+    }
+}
