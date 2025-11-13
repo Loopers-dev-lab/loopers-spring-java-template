8a9751c5535710eb9b215106ee1ff3111928d438
junoade <ajchoi0928@gmail.com>
2025-11-13 23:35:27 +0900

    test: ProductModel 엔티티 생성 테스트 작성

--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductModel.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductModel.java
@@ -2,7 +2,11 @@ package com.loopers.domain.product;
 
 import com.loopers.domain.BaseEntity;
 import com.loopers.domain.brand.BrandModel;
+import com.loopers.domain.brand.BrandStatus;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +17,7 @@ import lombok.Setter;
 public class ProductModel extends BaseEntity {
 
     @Column(nullable = false, length = 50)
-    private String productName;
+    private String name;
 
     @Column(nullable = false, length = 32)
     private String category;
@@ -22,9 +26,74 @@ public class ProductModel extends BaseEntity {
 
     private Integer stock;
 
-    private Character status;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ProductStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "brand_id", nullable = false)
     private BrandModel brand;
+
+    protected ProductModel() {}
+
+    @Builder
+    public ProductModel(String name,
+                        String category,
+                        Integer price,
+                        Integer stock,
+                        ProductStatus status,
+                        BrandModel brand) {
+
+        validateProductName(name);
+        validateProductCategory(category);
+        validateProductStatus(status);
+        validatePrice(price);
+        validateStock(stock);
+
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+        this.status = status;
+        this.brand = brand;
+    }
+
+    private void validateProductCategory(String category) {
+        if(category == null || category.isBlank()) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "카테고리명은 필수 입력값 입니다.");
+        }
+
+        if(category.length() > 32) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "카테고리명은 32자 이하 입력값 입니다.");
+        }
+    }
+
+    private void validateProductName(String name) {
+        if(name == null || name.isBlank()) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "상품명은 필수 입력값 입니다.");
+        }
+
+        if(name.length() > 50) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "상품명은 50자 이하 입력값이어야 합니다.");
+        }
+    }
+
+    private void validateProductStatus(ProductStatus status) {
+        if(status == null) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "상태값 필수 입력");
+        }
+    }
+
+    private void validatePrice(Integer price) {
+        if(price == null || price < 0) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "상품 금액은 비거나 음수일 수 업습니다.");
+        }
+    }
+
+    private void validateStock(Integer stock) {
+        if(stock == null || stock < 0) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "상품 재고는 비거나 음수일 수 업습니다.");
+        }
+    }
+
 }
---
+++ apps/commerce-api/src/test/java/com/loopers/domain/product/ProductModelTest.java
@@ -0,0 +1,128 @@
+package com.loopers.domain.product;
+
+import com.loopers.domain.brand.BrandModel;
+import com.loopers.domain.brand.BrandStatus;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ProductModelTest {
+    @DisplayName("ProductModel 생성 테스트")
+    @Nested
+    class Create {
+
+        private String name;
+        private String category;
+        private Integer price;
+        private Integer stock;
+        private ProductStatus status;
+        private BrandModel brand;
+
+
+        @BeforeEach
+        void setUp() {
+            name = "test_product";
+            category = "test_category";
+            price = 100;
+            stock = 100;
+            status = ProductStatus.ON_SALE;
+            brand = new BrandModel("TEST", "TEST", BrandStatus.REGISTERED);
+        }
+
+
+        @Test
+        @DisplayName("1자 이상 50자 이하의 상품이름이 들어오면 ProductModel 생성에 성공한다")
+        void create_whenValidProductNameIsGiven() {
+            // act
+            ProductModel product = ProductModel.builder()
+                    .name(name)
+                    .category(category)
+                    .price(price)
+                    .stock(stock)
+                    .status(status)
+                    .brand(brand).build();
+
+            // assert
+            assertAll(
+                    () -> assertThat(product.getId()).isNotNull(),
+                    () -> assertThat(product.getName()).isEqualTo(name),
+                    () -> assertThat(product.getCategory()).isEqualTo(category),
+                    () -> assertThat(product.getStatus()).isEqualTo(status),
+                    () -> assertThat(product.getBrand()).isEqualTo(brand)
+            );
+        }
+
+        @Test
+        @DisplayName("상품이름이 50자 초과되면 에러를 반환한다.")
+        void throwsBadException_whenTooLongProductNameIsGiven() {
+            // given
+            name = "a".repeat(51);
+
+            // when
+            CoreException result = assertThrows(CoreException.class, () -> {
+                ProductModel.builder()
+                        .name(name)
+                        .category(category)
+                        .price(price)
+                        .stock(stock)
+                        .status(status)
+                        .brand(brand).build();
+            });
+
+            // then
+            AssertionsForClassTypes.assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+            AssertionsForClassTypes.assertThat(result.getMessage()).isEqualTo("상품명은 50자 이하 입력값이어야 합니다.");
+        }
+
+        @Test
+        @DisplayName("상품이름이 null이면 에러를 반환한다.")
+        void throwsBadException_whenProductNameIsNull() {
+            // given
+            name = "";
+
+            // when
+            CoreException result = assertThrows(CoreException.class, () -> {
+                ProductModel.builder()
+                        .name(name)
+                        .category(category)
+                        .price(price)
+                        .stock(stock)
+                        .status(status)
+                        .brand(brand).build();
+            });
+
+            // then
+            AssertionsForClassTypes.assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @Test
+        @DisplayName("상품이름이 공백이면 에러를 반환한다.")
+        void throwsBadException_whenBrandNameIsSpace() {
+            // given
+            name = " ";
+
+            // when
+            CoreException result = assertThrows(CoreException.class, () -> {
+                ProductModel.builder()
+                        .name(name)
+                        .category(category)
+                        .price(price)
+                        .stock(stock)
+                        .status(status)
+                        .brand(brand).build();
+            });
+
+            // then
+            AssertionsForClassTypes.assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+    }
+}
