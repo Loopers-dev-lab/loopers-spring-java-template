f75423ae6602e310f32e6274ed3e836a0446a38f
jeonga1022 <pja1048@gmail.com>
2025-11-13 23:57:58 +0900

    test: 사용자 ID와 상품 ID가 주어지면, 좋아요가 등록된다.

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/like/ProductLikeTest.java
@@ -0,0 +1,31 @@
+package com.loopers.domain.like;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ProductLikeTest {
+
+    private static final Long USER_ID = 1L;
+    private static final Long PRODUCT_ID = 1L;
+
+    @DisplayName("좋아요 객체 생성")
+    @Nested
+    class Like {
+
+        @DisplayName("사용자 ID와 상품 ID가 주어지면, 좋아요가 객체가 생성된다")
+        @Test
+        void likeTest1() {
+            ProductLike like = ProductLike.create(USER_ID, PRODUCT_ID);
+
+            assertAll(
+                    () -> assertThat(like.getUserId()).isEqualTo(USER_ID),
+                    () -> assertThat(like.getProductId()).isEqualTo(PRODUCT_ID),
+                    () -> assertThat(like.getDeletedAt()).isNull()
+            );
+        }
+    }
+}
