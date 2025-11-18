48b4b1fce6cd5f5793970623565ce5cf9bfc5dd5
Seoyeon Lee <68765200+sylee6529@users.noreply.github.com>
2025-11-19 08:32:57 +0900

    feat: REST API 테스트용 RestPage 헬퍼 클래스 추가
    Spring Data Page 인터페이스의 JSON 역직렬화 문제 해결
    
    문제:
    - Page는 추상 인터페이스로 Jackson이 인스턴스 생성 불가
    - TestRestTemplate의 Page<T> 응답 변환 시 InvalidDefinitionException 발생
    
    해결:
    - PageImpl을 상속한 구체 클래스 생성
    - @JsonCreator로 JSON 필드를 생성자 파라미터에 매핑
    - E2E 테스트에서 Page 대신 RestPage 사용 가능

---
+++ apps/commerce-api/src/test/java/com/loopers/utils/RestPage.java
@@ -0,0 +1,45 @@
+package com.loopers.utils;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * RestTemplate에서 Page 객체를 역직렬화하기 위한 헬퍼 클래스
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RestPage<T> extends PageImpl<T> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public RestPage(@JsonProperty("content") List<T> content,
+                    @JsonProperty("number") int number,
+                    @JsonProperty("size") int size,
+                    @JsonProperty("totalElements") Long totalElements,
+                    @JsonProperty("pageable") JsonNode pageable,
+                    @JsonProperty("last") boolean last,
+                    @JsonProperty("totalPages") int totalPages,
+                    @JsonProperty("sort") JsonNode sort,
+                    @JsonProperty("first") boolean first,
+                    @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
+    public RestPage(List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public RestPage(List<T> content) {
+        super(content);
+    }
+
+    public RestPage() {
+        super(new ArrayList<>());
+    }
+}
