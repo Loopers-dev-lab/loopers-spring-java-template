6e14e9decd604ddfead4883cacc83106c6a51b1c
BonSeung <rnqhstmd9134@naver.com>
2025-11-18 22:45:43 +0900

    feat: Product 조회 시 비관적 락 사용한 메서드 추가

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductJpaRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductJpaRepository.java
@@ -1,6 +1,8 @@
 package com.loopers.infrastructure.product;
 
 import com.loopers.domain.product.Product;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 
@@ -18,4 +21,12 @@ public interface ProductJpaRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p JOIN FETCH p.brand b " +
             "WHERE (:brandId IS NULL OR b.id = :brandId)")
     Page<Product> findProducts(@Param("brandId") Long brandId, Pageable pageable);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Optional<Product> findByIdWithPessimisticLock(@Param("id") Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id IN :ids")
+    List<Product> findAllByIdsWithPessimisticLock(@Param("ids") Collection<Long> ids);
 }
