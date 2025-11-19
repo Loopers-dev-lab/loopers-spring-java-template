4265410a64508c9898fa0e6fcefd381a76371037
HongChangMo <hcm7402@naver.com>
2025-11-20 08:22:14 +0900

    refactor: 주문 생성 기능 도메인 서비스 -> Facade 로 이관
    - 기존에 도메인 서비스에 있던 특정 로직이 도메인 서비스 역할과 책임에서 벗어나, 해당 로직은 여러 도메인 서비스 호출 및 조율을 담당하는 Facade 레이어에 있어야 한다고 판단하여 리팩토링

--- apps/commerce-api/src/main/java/com/loopers/domain/orderitem/OrderItemService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/orderitem/OrderItemService.java
@@ -11,12 +11,6 @@ public class OrderItemService {
 
     private final OrderItemRepository orderItemRepository;
 
-    /**
-     * 특정 주문의 주문 항목들을 조회합니다.
-     *
-     * @param orderId 주문 ID
-     * @return 주문 항목 리스트
-     */
     public List<OrderItem> getOrderItemsByOrder(Long orderId) {
         return orderItemRepository.getOrderItemsByOrder(orderId);
     }
