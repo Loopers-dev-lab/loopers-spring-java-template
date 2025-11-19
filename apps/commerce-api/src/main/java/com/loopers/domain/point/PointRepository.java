09ba79639e1c6784952fd4ec5a0435fc7d90bd98
adminhelper <201401339hs@gmail.com>
2025-11-20 04:24:15 +0900

    fix : 비관적락 명시

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointRepository.java
@@ -1,10 +1,17 @@
 package com.loopers.domain.point;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface PointRepository {
 
-    Optional<Point> findByUserId(String userId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Point s WHERE s.id= :id")
+    Optional<Point> findByUserId(@Param("id") String userId);
 
     Point save(Point point);
 }
