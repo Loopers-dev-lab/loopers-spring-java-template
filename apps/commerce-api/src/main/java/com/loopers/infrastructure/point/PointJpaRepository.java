cc1207ca3bd4628573e5d1ffe03ee1828f4a94ae
pmh5574 <g_a_m_e_@naver.com>
2025-11-25 11:32:56 +0900

    fix: import Param spring으로 변경

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/point/PointJpaRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/point/PointJpaRepository.java
@@ -1,16 +1,16 @@
 package com.loopers.infrastructure.point;
 
-import static jakarta.persistence.LockModeType.PESSIMISTIC_WRITE;
 
 import com.loopers.domain.point.Point;
-import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PointJpaRepository extends JpaRepository<Point, Long> {
-    @Lock(PESSIMISTIC_WRITE)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM Point p WHERE p.userId = :userId")
     Optional<Point> findByUserIdWithLock(@Param("userId") Long userId);
 
--- apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductJpaRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductJpaRepository.java
@@ -1,22 +1,22 @@
 package com.loopers.infrastructure.product;
 
 import com.loopers.domain.product.Product;
-import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
-       SELECT p 
-       FROM Product p 
-       WHERE p.id IN :ids 
-       ORDER BY p.id 
-       """)
+            SELECT p 
+            FROM Product p 
+            WHERE p.id IN :ids 
+            ORDER BY p.id 
+            """)
     List<Product> findAllByIdInWithLock(@Param("ids") List<Long> ids);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
