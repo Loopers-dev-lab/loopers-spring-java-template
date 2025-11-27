13915281a9cf19a07b332e1496d18a8df011fbd0
green <green@mobidoc.us>
2025-11-27 22:31:13 +0900

    feat: 테스트용 NoOpRedisCacheTemplate 추가
    NoOpRedisCacheTemplate 클래스:
    - Redis 캐시를 사용하지 않고 항상 로더를 실행하는 테스트용 캐시 템플릿 구현
    - @Profile("test") 어노테이션을 통해 테스트 환경에서만 활성화
    
    build.gradle.kts:
    - Jackson Databind 의존성 추가
    
    IntegrationTestSupport:
    - ActiveProfiles("test") 추가하여 테스트 프로파일 설정
    
    ProductFacadeIntegrationTest:
    - RedisCacheTemplate 관련 코드 제거 및 테스트 로직 정리

--- apps/commerce-api/src/test/java/com/loopers/support/test/IntegrationTestSupport.java
+++ apps/commerce-api/src/test/java/com/loopers/support/test/IntegrationTestSupport.java
@@ -4,8 +4,10 @@ import com.loopers.utils.DatabaseCleanUp;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public abstract class IntegrationTestSupport {
 
   @Autowired
