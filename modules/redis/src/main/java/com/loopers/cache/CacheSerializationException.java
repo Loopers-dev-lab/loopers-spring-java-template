9b74d349c3178d3501674e81bdadc1fb9a6e38eb
green <green@mobidoc.us>
2025-12-05 14:07:35 +0900

    refactor: 캐시 인프라 개선
    RedisCacheTemplate:
    - 직렬화 예외 처리 개선
    - CacheSerializationException 추가
    
    ResilientCacheTemplate:
    - Resilience4j CircuitBreaker 적용
    - Redis 장애 시 Fallback 처리

---
+++ modules/redis/src/main/java/com/loopers/cache/CacheSerializationException.java
@@ -0,0 +1,8 @@
+package com.loopers.cache;
+
+public class CacheSerializationException extends RuntimeException {
+
+  public CacheSerializationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
