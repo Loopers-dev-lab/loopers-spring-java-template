560a9578a507e6e2b5cedfd11b5e19f8bcb6eacc
Yim Yoobin <yoobinbkk@naver.com>
2025-12-05 16:52:14 +0900

    [Round 6] resilience4j 적용

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/PgFeignClient.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/PgFeignClient.java
@@ -1,6 +1,9 @@
 package com.loopers.domain.payment;
 
 import com.loopers.interfaces.api.ApiResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(name = "pgClient", url = "${pg.api.url}")
 public interface PgFeignClient {
 
+    @Retry(name = "pg-api")
+    @CircuitBreaker(name = "pg-api")
+    @RateLimiter(name = "pg-api")
     @PostMapping("/api/v1/payments")
     ApiResponse<PaymentDto.PgResponse> approvePayment(
             @RequestHeader("X-USER-ID") Long userId,
