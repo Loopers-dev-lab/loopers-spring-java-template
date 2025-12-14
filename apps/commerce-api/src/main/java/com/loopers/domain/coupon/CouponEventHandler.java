7a444fb5b234950e8e38bb3a415b6f7fcb0cd2c5
hyujikoh <dvum0045@gmail.com>
2025-12-14 22:53:43 +0900

    refactor(coupon): 쿠폰 사용 이벤트 비동기 처리 제거

--- apps/commerce-api/src/main/java/com/loopers/domain/coupon/CouponEventHandler.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/coupon/CouponEventHandler.java
@@ -37,7 +37,6 @@ public class CouponEventHandler {
      * 쿠폰 사용 실패 시 전체 주문 트랜잭션 롤백
      */
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
-    @Async
     public void handleCouponConsume(CouponConsumeEvent event) {
         Objects.requireNonNull(event, "쿠폰 이벤트가 null 입니다.");
 
