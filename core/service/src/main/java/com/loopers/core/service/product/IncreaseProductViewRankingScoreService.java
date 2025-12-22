fabc903bb496bf437a67a8eb96c0450419e6daa7
kilian <audrb96@gmail.com>
2025-12-22 22:54:51 +0900

    feature : 상품 조회 랭킹 점수 증가 구현

--- core/service/src/main/java/com/loopers/core/service/product/IncreaseProductViewRankingScoreService.java
+++ core/service/src/main/java/com/loopers/core/service/product/IncreaseProductViewRankingScoreService.java
@@ -1,9 +1,9 @@
-package com.loopers.core.service.productlike;
+package com.loopers.core.service.product;
 
 import com.loopers.core.domain.product.repository.ProductRankingCacheRepository;
 import com.loopers.core.domain.product.vo.ProductId;
 import com.loopers.core.service.config.InboxEvent;
-import com.loopers.core.service.productlike.command.IncreaseProductLikeRankingScoreCommand;
+import com.loopers.core.service.product.command.IncreaseProductViewRankingScoreCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,20 +12,20 @@ import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-public class IncreaseProductLikeRankingScoreService {
+public class IncreaseProductViewRankingScoreService {
 
     private final ProductRankingCacheRepository productRankingCacheRepository;
 
-    @Value("${product.ranking.score.weight.like}")
+    @Value("${product.ranking.score.weight.view}")
     private double weight;
 
     @InboxEvent(
             aggregateType = "PRODUCT",
-            eventType = "INCREASE_PRODUCT_LIKE_RANKING_SCORE",
+            eventType = "INCREASE_PRODUCT_VIEW_RANKING_SCORE",
             eventIdField = "eventId",
             aggregateIdField = "productId"
     )
-    public void increase(IncreaseProductLikeRankingScoreCommand command) {
+    public void increase(IncreaseProductViewRankingScoreCommand command) {
         ProductId productId = new ProductId(command.productId());
         productRankingCacheRepository.increaseDaily(productId, LocalDateTime.now(), weight);
     }
