328b783850b9875676496234ac3cbeb52f9e2fac
BonSeung <rnqhstmd9134@naver.com>
2025-12-29 16:34:02 +0900

    feat: validation import 추가

--- apps/commerce-streamer/build.gradle.kts
+++ apps/commerce-streamer/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     // web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // querydsl
     annotationProcessor("com.querydsl:querydsl-apt::jakarta")
