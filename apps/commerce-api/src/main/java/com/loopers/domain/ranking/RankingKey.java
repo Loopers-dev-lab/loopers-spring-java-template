151393f661231506a2c86778f74462e8f67db42a
BonSeung <rnqhstmd9134@naver.com>
2025-12-29 16:36:44 +0900

    refactor: 사용하지 않는 메서드 제거

--- apps/commerce-api/src/main/java/com/loopers/domain/ranking/RankingKey.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/ranking/RankingKey.java
@@ -17,16 +17,4 @@ public class RankingKey {
     public static String daily(LocalDate date) {
         return KEY_PREFIX + date.format(DATE_FORMATTER);
     }
-
-    public static String today() {
-        return daily(LocalDate.now());
-    }
-
-    public static String fromDateString(String dateString) {
-        return KEY_PREFIX + dateString;
-    }
-
-    public static LocalDate parseDate(String dateString) {
-        return LocalDate.parse(dateString, DATE_FORMATTER);
-    }
 }
--- apps/commerce-streamer/src/main/java/com/loopers/domain/ranking/RankingKey.java
+++ apps/commerce-streamer/src/main/java/com/loopers/domain/ranking/RankingKey.java
@@ -17,32 +17,4 @@ public class RankingKey {
     public static String daily(LocalDate date) {
         return KEY_PREFIX + date.format(DATE_FORMATTER);
     }
-
-    /**
-     * 오늘 랭킹 키 생성
-     */
-    public static String today() {
-        return daily(LocalDate.now());
-    }
-
-    /**
-     * 어제 랭킹 키 생성
-     */
-    public static String yesterday() {
-        return daily(LocalDate.now().minusDays(1));
-    }
-
-    /**
-     * 날짜 문자열(yyyyMMdd)로부터 키 생성
-     */
-    public static String fromDateString(String dateString) {
-        return KEY_PREFIX + dateString;
-    }
-
-    /**
-     * 키에서 날짜 추출
-     */
-    public static String extractDate(String key) {
-        return key.replace(KEY_PREFIX, "");
-    }
 }
